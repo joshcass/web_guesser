6af55b7b289691e02f8a78a7edd955dbdb262f86
Josh Cass <joshuacass@gmail.com>
2015-05-11 17:00:35 -0600

    generates random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+class Generator
+  attr_reader :num
+
+  def initialize
+    @num = rand(100)
+  end
+end
+
+gen = Generator.new
 
 get '/' do
-  'Hello World!'
+  "The SECRET NUMBER is #{gen.num}"
 end
