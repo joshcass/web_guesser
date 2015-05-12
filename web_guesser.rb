af208a233e97d3c7d287921e4cf7aa25542b3658
Josh Cass <joshuacass@gmail.com>
2015-05-12 12:00:03 -0600

    switching comps

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,16 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-class Generator
-  attr_reader :num
-
-  def initialize
-    @num = rand(100)
-  end
-end
-
-number = Generator.new.num
+set :num, rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+  msg, color = check_guess(guess)
+  erb :index, :locals => {:number => settings.num, :msg => msg, :color => color}
+end
+
+def check_guess(guess)
+  if guess > settings.num + 5
+    ['Way too high!', 'red']
+  elsif guess > settings.num
+    ['Too high!', 'salmon']
+  elsif guess < settings.num - 5
+    ['Way too low!', 'red']
+  elsif guess < settings.num
+    ['Too low!', 'salmon']
+  else guess == settings.num
+    ["You got it right!<br />The SECRET NUMBER is #{settings.num}", 'green']
+  end
 end
