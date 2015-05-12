bd608be3a8f724bd2e58c1f73d2296d35d6ba7ab
Josh Cass <joshuacass@gmail.com>
2015-05-12 13:17:39 -0600

    back to this later

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,10 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :num, rand(100)
+@@guess_num = 5
 
 get '/' do
   guess = params['guess'].to_i
-  msg, color = check_guess(guess)
+  msg, color = guess_number(guess)
   erb :index, :locals => {:number => settings.num, :msg => msg, :color => color}
 end
 
@@ -19,6 +20,22 @@ def check_guess(guess)
   elsif guess < settings.num
     ['Too low!', 'salmon']
   else guess == settings.num
+    guess_reset
     ["You got it right!<br />The SECRET NUMBER is #{settings.num}", 'green']
   end
+end
+
+def guess_number(guess)
+  if @@guess_num > 0
+    @@guess_num -= 1
+    check_guess(guess)
+  elsif @@guess_num == 0
+    guess_reset
+    ["You ran out of guesses!<br />There's a new secret number, try again!", 'brown']
+  end
+end
+
+def guess_reset
+  @@guess_num = 5
+
 end
